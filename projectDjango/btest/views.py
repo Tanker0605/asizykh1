14adc907c1fe50a131aa7c9a9037edd11c1470fa
My Name <myEmail@example.com>
2023-10-23 20:15:21 +0300

    добавление админки

--- projectDjango/btest/views.py
+++ projectDjango/btest/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
 # Create your views here.
 from .models import *
-
-
-
+from django.shortcuts import render
 def index(request):
-    bbs = Bb.objects.order_by('-published')
-    return render(request, "btest/index.html", {'bbs': bbs})
+    bbs = Bb.objects.all()
+    rs = Rubric.objects.all()
+    return render(request, "btest/index.html", {'bbs': bbs, 'rs': rs})
+
+
